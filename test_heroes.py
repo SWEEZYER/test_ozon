a5d3a50ca07a654de6e3301734a8db96bef164ff
SWEEZYER <maxgrad1993@mail.ru>
2025-10-20 17:22:16 +0300

    v1.0

--- test_heroes.py
+++ test_heroes.py
@@ -6,24 +6,30 @@ def test_male_with_occupation():
     expected_name = "Galactus"
     assert find_tallest_hero("Male", True) == expected_name
 
+
 def test_male_no_occupation():
     expected_name = "Fin Fang Foom"
     assert find_tallest_hero("Male", False) == expected_name
+
 
 def test_female_with_occupation():
     expected_name = "Wolfsbane"
     assert find_tallest_hero("Female", True) == expected_name
 
+
 def test_female_no_occupation():
     expected_name = "Ardina"
     assert find_tallest_hero("Female", False) == expected_name
+
 
 def test_other_gender_with_occupation():
     expected_name = "Living Brain"
     assert find_tallest_hero("-", True) == expected_name
 
+
 def test_no_results_invalid_gender():
     assert find_tallest_hero("Kryptonian", True) is None
+
 
 def test_no_results_valid_filters():
     assert find_tallest_hero("Genderless", False) is None
